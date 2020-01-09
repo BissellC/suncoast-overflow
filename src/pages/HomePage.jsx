497a8eb94e3c27776cb9c7b1489affdabc717064
Charlie Bissell <bissell31@gmail.com>
2020-01-09 16:34:16 -0500

    Mapping questions and answers, working votes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,46 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import NavBar from '../components/NavBar'
+import axios from 'axios'
 
 const HomePage = () => {
+  const [questions, setQuestions] = useState([])
+
+  const getQuestions = async () => {
+    const resp = await axios.get('https://localhost:5001/api/Question')
+    setQuestions(resp.data)
+  }
+
+  useEffect(() => {
+    getQuestions()
+  }, [])
+
   return (
     <>
       <NavBar />
 
       <main>
         <h1>Questions</h1>
-
-        <section className="question-container">
-          <div>
-            <div>
-              <p className="question-score">0</p>
-              <p className="question-score-text">Score</p>
-            </div>
-            <p className="question-title">How do I send an eMail?</p>
-          </div>
-          <p className="question-timestamp">Jan 7, 2019 10:10am</p>
-        </section>
-
-        <section className="question-container">
-          <div>
-            <div>
-              <p className="question-score">0</p>
-              <p className="question-score-text">Score</p>
-            </div>
-            <p className="question-title">How do I send an eMail?</p>
-          </div>
-          <p className="question-timestamp">Jan 7, 2019 10:10am</p>
-        </section>
-        <section className="question-container">
-          <div>
-            <div>
-              <p className="question-score">0</p>
-              <p className="question-score-text">Score</p>
-            </div>
-            <p className="question-title">How do I send an eMail?</p>
-          </div>
-          <p className="question-timestamp">Jan 7, 2019 10:10am</p>
-        </section>
+        {questions.map(question => {
+          return (
+            <Link to={'/question/' + question.id}>
+              <section className="question-container">
+                <div>
+                  <div>
+                    <p className="question-score">{question.voteCount}</p>
+                    <p className="question-score-text">Score</p>
+                  </div>
+                  <p className="question-title">{question.title}</p>
+                </div>
+                <p className="question-timestamp">{question.timestamp}</p>
+              </section>
+            </Link>
+          )
+        })}
       </main>
     </>
   )
--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -1,55 +1,134 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import NavBar from '../components/NavBar'
+import axios from 'axios'
 
-const QuestionPage = () => {
+const QuestionPage = props => {
+  const [question, setQuestion] = useState([])
+  const [questionCount, setQuestionCount] = useState()
+  const [answerCount, setAnswerCount] = useState()
+  const [answers, setAnswers] = useState([])
+
+  const getQuestion = async () => {
+    const resp = await axios.get(
+      'https://localhost:5001/api/Question/' + props.match.params.id
+    )
+    setQuestion(resp.data)
+    setQuestionCount(resp.data.voteCount)
+    setAnswers(resp.data.answers)
+  }
+
+  const upvoteQ = async () => {
+    const resp = await axios.put(
+      'https://localhost:5001/api/Question/' + props.match.params.id,
+      {
+        id: question.id,
+        title: question.title,
+        body: question.body,
+        description: question.description,
+        timeStamp: question.timeStamp,
+        voteCount: questionCount + 1,
+      }
+    )
+    getQuestion()
+  }
+
+  const downvoteQ = async () => {
+    const resp = await axios.put(
+      'https://localhost:5001/api/Question/' + props.match.params.id,
+      {
+        id: question.id,
+        title: question.title,
+        body: question.body,
+        description: question.description,
+        timeStamp: question.timeStamp,
+        voteCount: questionCount - 1,
+      }
+    )
+    getQuestion()
+  }
+
+  const upvoteA = async answer => {
+    const resp = await axios.put(
+      'https://localhost:5001/api/Answer/' + answer.id,
+      {
+        id: answer.id,
+        body: answer.body,
+        timeStamp: answer.timeStamp,
+        voteCount: answer.voteCount + 1,
+        questionID: question.id,
+      }
+    )
+    getQuestion()
+  }
+
+  const downvoteA = async answer => {
+    const resp = await axios.put(
+      'https://localhost:5001/api/Answer/' + answer.id,
+      {
+        id: answer.id,
+        body: answer.body,
+        timeStamp: answer.timeStamp,
+        voteCount: answer.voteCount - 1,
+        questionID: question.id,
+      }
+    )
+    getQuestion()
+  }
+
+  useEffect(() => {
+    getQuestion()
+  }, [])
+
   return (
     <>
       <NavBar />
 
       <main>
         <section className="question">
-          <p className="question-title">How do I send an eMail?</p>
+          <p className="question-title">{question.title}</p>
           <div className="question-body-container">
             <div className="question-vote-container">
               <p className="zoom">
-                <i class="fas fa-caret-up"></i>
+                <i onClick={upvoteQ} class="fas fa-caret-up"></i>
               </p>
-              <p className="question-score">0</p>
+              <p className="question-score">{question.voteCount}</p>
               <p className="zoom">
-                <i class="fas fa-caret-down"></i>
+                <i onClick={downvoteQ} class="fas fa-caret-down"></i>
               </p>
             </div>
-            <p className="question-body">
-              This is my first time on the internet, please help! Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Dicta consequuntur
-              blanditiis doloremque voluptatum, aliquid quaerat libero sit
-              debitis eligendi voluptates, cumque et explicabo nostrum veritatis
-              minus vel amet maxime numquam!
-            </p>
+            <p className="question-body">{question.body}</p>
           </div>
-          <p className="question-timestamp">Jan 7, 2019 10:10am</p>
+          <p className="question-timestamp">{question.timeStamp}</p>
         </section>
 
         <section className="answer">
           <h1 className="answer-text">Answers</h1>
-          <section className="answer-container">
-            <div className="question-body-container">
-              <div className="question-vote-container">
-                <p className="zoom">
-                  <i class="fas fa-caret-up"></i>
-                </p>
-                <p className="question-score">0</p>
-                <p className="zoom">
-                  <i class="fas fa-caret-down"></i>
-                </p>
-              </div>
-              <p className="question-body">
-                This is my first time on the internet, please help!
-              </p>
-            </div>
-            <p className="question-timestamp">Jan 7, 2019 10:10am</p>
-          </section>
+          {answers.map(answer => {
+            return (
+              <section className="answer-container">
+                <div className="question-body-container">
+                  <div className="question-vote-container">
+                    <p className="zoom">
+                      <i
+                        onClick={() => upvoteA(answer)}
+                        class="fas fa-caret-up"
+                      ></i>
+                    </p>
+                    <p className="question-score">{answer.voteCount}</p>
+                    <p className="zoom">
+                      <i
+                        onClick={() => downvoteA(answer)}
+                        class="fas fa-caret-down"
+                      ></i>
+                    </p>
+                  </div>
+                  <p className="question-body">{answer.body}</p>
+                </div>
+                <p className="question-timestamp">{answer.timeStamp}</p>
+              </section>
+            )
+          })}
         </section>
 
         <section className="your-answer">
